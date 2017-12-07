5195e92234234cf6a8d92f08d67a790e75ad7c5d
song <764469385@qq.com>
2017-12-07 19:47:08 +0800

    list demo

--- app/src/main/java/org/song/demo/MainActivity.java
+++ app/src/main/java/org/song/demo/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.preference.ListPreference;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
--- app/src/main/java/org/song/demo/RecyVideoActivity.java
+++ app/src/main/java/org/song/demo/RecyVideoActivity.java
@@ -2,18 +2,16 @@ package org.song.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
-import android.widget.ListView;
 
 import org.song.demo.listvideo.CallBack;
 import org.song.demo.listvideo.ListCalculator;
-import org.song.demo.listvideo.ListViewGetter;
+import org.song.demo.listvideo.RecyclerViewGetter;
 import org.song.videoplayer.ConfigManage;
 import org.song.videoplayer.DemoQSVideoView;
 import org.song.videoplayer.IVideoPlayer;
@@ -23,43 +21,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class ListVideoActivity extends AppCompatActivity implements CallBack {
+public class RecyVideoActivity extends AppCompatActivity implements CallBack {
 
-    ListView listView;
+    RecyclerView recyclerView;
     List<String> data = new ArrayList<>();
     ListCalculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_video);
-        listView = (ListView) findViewById(R.id.recyclerview);
-        //listView.setLayoutManager(new LinearLayoutManager(this));
+        setContentView(R.layout.activity_recy_video);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         for (int i = 0; i < 100; i++)
             data.add("这是一个标题" + i + ",http://videos.kpie.com.cn/videos/20170526/037DCE54-EECE-4520-AA92-E4002B1F29B0.mp4");
 
+        recyclerView.setAdapter(new Adapter(data));
 
-        calculator = new ListCalculator(new ListViewGetter(listView), this);
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+        calculator = new ListCalculator(new RecyclerViewGetter((LinearLayoutManager) recyclerView.getLayoutManager(), recyclerView), this);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            int newState = 0;
+
             @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-                this.newState = scrollState;
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                this.newState = newState;
                 if (newState == RecyclerView.SCROLL_STATE_IDLE)
                     calculator.onScrolled(300);
             }
 
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 calculator.onScrolling(newState);
 
             }
-
-            int newState = 0;
-
         });
-
-        listView.setAdapter(new Adapter(data));
-
     }
 
     DemoQSVideoView demoQSVideoView;
@@ -92,8 +88,7 @@ public class ListVideoActivity extends AppCompatActivity implements CallBack {
         super.onBackPressed();
     }
 
-
-    class Adapter extends BaseAdapter {
+    class Adapter extends RecyclerView.Adapter<Holder> {
 
         List<String> data;
 
@@ -102,49 +97,29 @@ public class ListVideoActivity extends AppCompatActivity implements CallBack {
         }
 
         @Override
-        public int getCount() {
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new Holder(View.inflate(RecyVideoActivity.this, R.layout.item_video, null));
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.bindData(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
             return data.size();
-        }
-
-        @Override
-        public String getItem(int position) {
-            return data.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            Holder h = null;
-            if (convertView == null)
-                h = new Holder(View.inflate(ListVideoActivity.this, R.layout.item_video, null));
-            else
-                h = (Holder) convertView.getTag();
-            h.position = position;
-
-            String[] arr = getItem(position).split(",");
-            h.qsVideoView.setUp(arr[1], arr[0]);
-            h.qsVideoView.getCoverImageView().setImageResource(R.mipmap.ic_launcher);
-            FrameLayout.LayoutParams l = new FrameLayout.LayoutParams(-1, (int) (((int) (Math.random() * 600) + 100) * getResources().getDisplayMetrics().density));
-            //qsVideoView.setLayoutParams(l);
-
-
-            return h.itemView;
         }
     }
 
 
-    class Holder {
-        int position;
+    class Holder extends RecyclerView.ViewHolder {
+
         DemoQSVideoView qsVideoView;
-        View itemView;
 
         Holder(View itemView) {
-            this.itemView = itemView;
-            itemView.setTag(this);
+            super(itemView);
+
             qsVideoView = (DemoQSVideoView) itemView.findViewById(R.id.qs);
             qsVideoView.setPlayListener(new PlayListener() {
                 @Override
@@ -161,7 +136,7 @@ public class ListVideoActivity extends AppCompatActivity implements CallBack {
                 public void onEvent(int what, Integer... extra) {
                     if (what == IVideoPlayer.EVENT_PREPARE_START) {
                         //ConfigManage.releaseOther(qsVideoView);
-                        calculator.setCurrentActiveItem(position);
+                        calculator.setCurrentActiveItem(getLayoutPosition());
                     }
                 }
             });
